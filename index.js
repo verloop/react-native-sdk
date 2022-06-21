2ab59cda7a6b311bbcf26db8f08873410dbb5a90
tarun-gujjula <tarun@verloop.io>
2022-06-21 19:06:47 +0530

    Updated react native 1.0.72-rc12

--- index.js
+++ index.js
@@ -5,8 +5,4 @@ const { RNVerloopSdk } = NativeModules;
 
 const VerloopSdk = RNVerloopSdk;
 
-// interface VerloopSdkInterface {
-//   createUserConfig(clientId: string,  userId:string ): void;
-// }
-
 export default VerloopSdk;
